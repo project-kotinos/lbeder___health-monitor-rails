340972588b2f42f51741e8e0e0d548e635b53b98
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-07 17:47:46 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -4,6 +4,8 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 gem install bundler
+gem install tzinfo
+gem install tzinfo-data
 bundle install --jobs=3 --retry=3
 bundle exec rake rubocop
 bundle exec rspec
